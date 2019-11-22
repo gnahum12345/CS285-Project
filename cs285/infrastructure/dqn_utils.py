wrong shapes :'(

cs285/infrastructure/dqn_utils.py
@@ -17,7 +17,7 @@ def get_env_kwargs(env_name):
         ),
         'q_func': agent_model,
         'replay_buffer_size': 10000,
-        'batch_size': 32,
+        'batch_size': 1,
         'gamma': 1.00,
         'learning_starts': 25,
         'learning_freq': 1,
@@ -56,9 +56,9 @@ def agent_model(obs, num_actions, scope, reuse=False):
             out = layers.conv2d(out, num_outputs=32, kernel_size=[8,8], stride=4, activation_fn=tf.nn.relu)
             out = layers.conv2d(out, num_outputs=64, kernel_size=[4,4], stride=2, activation_fn=tf.nn.relu)
             out = layers.conv2d(out, num_outputs=64, kernel_size=[3,3], stride=1, activation_fn=tf.nn.relu)
-            out = layers.fully_connected(out, num_outputs=512, activation_fn=tf.tanh)
-            out = layers.fully_connected(out, num_outputs=256)
-        return out
+            out = tf.layers.dense(inputs=out, units=512, activation=tf.tanh)
+            out = tf.layers.dense(inputs=out, units=256, activation=tf.tanh)
+            return out
 
 def agent_exploration_schedule(num_timesteps):
     return PiecewiseSchedule(
@@ -364,7 +364,11 @@ class MemoryOptimizedReplayBuffer(object):
         else:
             # this optimization has potential to saves about 30% compute time \o/
             img_h, img_w = self.obs.shape[1], self.obs.shape[2]
+            #print(self.obs.shape)
+            #import pdb; pdb.set_trace()
+            #obs = np.expand_dims(self.obs, 3)
             return self.obs[start_idx:end_idx].transpose(1, 2, 0, 3).reshape(img_h, img_w, -1)
+            #return self.obs[start_idx:end_idx].transpose(1, 2, 0, 3).reshape(img_h, img_w, -1)
 
     def store_frame(self, frame):
         """Store a single frame in the buffer at the next available index, overwriting
@@ -381,6 +385,8 @@ class MemoryOptimizedReplayBuffer(object):
         idx: int
             Index at which the frame is stored. To be used for `store_effect` later.
         """
+        # import pdb; pdb.set_trace()
+
         if self.obs is None:
             self.obs      = np.empty([self.size] + list(frame.shape), dtype=np.float32)
             self.action   = np.empty([self.size],                     dtype=np.int32)
@@ -411,6 +417,6 @@ class MemoryOptimizedReplayBuffer(object):
         done: bool
             True if episode was finished after performing that action.
         """
-        self.action[idx] = action
+        self.action[idx] = action[0]
         self.reward[idx] = reward
         self.done[idx]   = done

cs285/infrastructure/env.py
@@ -10,7 +10,7 @@ class Env:
         self.action_shape = args['mask_shape']
         print('ActionShap: \n\n ', self.action_shape)
         self.action_mask = np.zeros(self.action_shape) # 256 lines of k-space. For radial its 360 degrees of freedom. This is the mask.
-        self.observation_shape = (2,256,256)
+        self.observation_shape = (256,256,2)
         self.action_space = list(range(256))
         self.ksp = args['ksp'][:,:,32:288]
         self.coord = args['coord']
@@ -32,6 +32,7 @@ class Env:
         return rand
 
     def get_image(self):
+        # import pdb; pdb.set_trace()
         visibile_data = self.ksp * self.action_mask
         assert self.ksp.shape == visibile_data.shape, 'Data should be the same shape'
         # total variation
@@ -41,7 +42,7 @@ class Env:
             return mr.app.TotalVariationRecon(visibile_data, mps, lamda, coord=self.coord).run() # requires coordinates
 
         if self.cartesian:
-            return sp.rss(sp.ifft(visibile_data),axes=0)
+            return sp.rss(sp.ifft(visibile_data),axes=0) # (256,256)
 
         dcf = (self.coord[...,0]**2 + self.coord[...,1]**2)**0.5
         img_grid = sp.nufft_adjoint(visibile_data * dcf, coord)
@@ -55,12 +56,14 @@ class Env:
     def reset(self):
         obs = self.get_image()
         self.action_mask = np.zeros(self.action_shape)
+        ret = np.stack((obs, self.action_mask)).transpose([1,2,0])
+
         self._reset_buffer()
         print(type(obs))
         self.state_buffer.append(obs)
         self.prev_step = np.zeros((256,256))
         self.prev_loss = 1/float('inf')
-        return obs
+        return ret
 
     def act(self, action):
         '''
@@ -90,9 +93,11 @@ class Env:
         done = reward < 0.005
 
         observation = self.prev_step
+        obs = np.stack((observation, self.action_mask))
+        obs = obs.transpose([1, 2, 0])
+        #import pdb; pdb.set_trace()
         self.state_buffer.append(observation)
-        # TODO: something during training.
-        return loss, reward, done
+        return obs, reward, done
 
     def eval(self):
         self.training = False